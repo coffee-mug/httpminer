312fd3fb853d340e6f5b8d7a91015e0fe4e8dc83
Lucas Kostka <lucas@fifty-five.com>
2021-10-20 12:47:05 +0200

    Feature: Filter out requests with extensions we're no interested in in the generated csv, Feature: add current location URL to generated csv

--- injected.js
+++ injected.js
@@ -10,45 +10,22 @@ setInterval(() => {
 */
 
 
-// TODO: refactor this, I'm just testing for now
-// const button = document.createElement('button');
-// button.textContent = "Download request in CSV";
-
-// button.style.bottom = "5vh";
-// button.style.right = "5vw";
-// button.style.position = "absolute";
-
-// button.addEventListener('click', e => {
-//   // We must communicate through postMessage
-//   window.postMessage({
-//     type: "FROM_PAGE",
-//     action: "getResults"
-//   })
-// })
-
-// document.body.appendChild(button);
-
 window.addEventListener('message', e => {
-  // if (e.data && e.data.action == "getResults" && e.data.results) {
-  //   let parsed = JSON.parse(e.data.results);
-  //   exportToCSV(parsed);
-  // }
+  if (e.data && e.data.action === "getDataLayer" && e.data.type === "FROM_CONTENT") {
+    //copy dataLayer from page
+    var dataLayerCopy = {};
+    var dataLayer = window.tc_vars;
 
-  if(e.data && e.data.action === "getDataLayer" && e.data.type === "FROM_CONTENT"){
-      //copy dataLayer from page
-      var dataLayerCopy = {};
-      var dataLayer = window.tc_vars;
-      
-      for (const variable in dataLayer) {
-          if (dataLayer[variable]) {
-              dataLayerCopy[variable] = dataLayer[variable];
-          }
+    for (const variable in dataLayer) {
+      if (dataLayer[variable]) {
+        dataLayerCopy[variable] = dataLayer[variable];
       }
-      //send copy of dataLayer
-      console.log("dataLayer from injected.js: ", dataLayerCopy)
-      window.postMessage({
-          type: "FROM_PAGE",
-          dL: JSON.stringify(dataLayerCopy)
-      });
+    }
+    //send copy of dataLayer
+    console.log("dataLayer from injected.js: ", dataLayerCopy)
+    window.postMessage({
+      type: "FROM_PAGE",
+      dL: JSON.stringify(dataLayerCopy)
+    });
   }
 });
